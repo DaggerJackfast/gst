c45322e38c1abbbd3cecd9e5c0bc171c36fefd7a
Aibek <aibek.abdykasymov@gmail.com>
2020-03-01 19:15:42 +0600

    Added methods to user repository

--- src/main.go
+++ src/main.go
@@ -19,7 +19,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	for _, u := range users{
+	for _, u := range users {
 		fmt.Println(u.Id, u.Email, u.Username, u.Password)
+	}
+	n, err := repo.Find(1)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(n.Id, n.Email, n.Username, n.Password)
+	m, err := repo.FindByEmail("ab@mail.com")
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(m.Id, m.Email, m.Username, m.Password)
+	m, err = repo.FindByUsername("ab")
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(m.Id, m.Email, m.Username, m.Password)
+	ur := User{Email:"dd@mail.com", Username:"dd", Password:"ddddd"}
+	user, err := repo.Store(&ur)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(user.Id, user.Email, user.Username, user.Password)
+	user.Email = "dd.new@mail.com"
+	user.Username = "dd.new"
+	err = repo.Update(user)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(user.Id, user.Email, user.Username, user.Password)
+	err = repo.Delete(user.Id)
+	if err != nil{
+		panic(err)
 	}
 }
--- src/user_repository.go
+++ src/user_repository.go
@@ -11,8 +11,8 @@ type UserRepository interface {
 	FindByUsername(username string) (*User, error)
 	All() ([]*User, error)
 	Update(user *User) error
-	Store(user *User) error
-	Add(user *User) error
+	Store(user *User) (*User, error)
+	Delete(id int) error
 }
 
 type userRepository struct {
@@ -25,13 +25,43 @@ func NewUserRepository(db sql.DB) *userRepository {
 	}
 }
 
-func (repo *userRepository) All() ([]User, error) {
+func (repo *userRepository) Find(id int) (*User, error) {
+	row := repo.db.QueryRow("select * from users where id=$1", id)
+	var user User
+	err := row.Scan(&user.Id, &user.Email, &user.Username, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (repo *userRepository) FindByEmail(email string) (*User, error) {
+	row := repo.db.QueryRow("select * from users where email=$1", email)
+	var user User
+	err := row.Scan(&user.Id, &user.Email, &user.Username, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (repo *userRepository) FindByUsername(username string) (*User, error) {
+	row := repo.db.QueryRow("select * from users where username=$1", username)
+	var user User
+	err := row.Scan(&user.Id, &user.Email, &user.Username, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (repo *userRepository) All() ([]*User, error) {
 	rows, err := repo.db.Query("select * from users order by email;")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var users []User
+	var users []*User
 	for rows.Next() {
 		u := User{}
 		err := rows.Scan(&u.Id, &u.Email, &u.Username, &u.Password)
@@ -39,7 +69,33 @@ func (repo *userRepository) All() ([]User, error) {
 			fmt.Println(err)
 			continue
 		}
-		users = append(users, u)
+		users = append(users, &u)
 	}
 	return users, nil
+}
+
+func (repo *userRepository) Update(user *User) error {
+	_, err := repo.db.Exec("update users set email=$2, username=$3, password=$4 where id=$1",
+		user.Id, user.Email, user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *userRepository) Store(user *User) (*User, error) {
+	err := repo.db.QueryRow("insert into users (email, username, password) values($1, $2, $3) returning  id",
+		user.Email, user.Username, user.Password).Scan(&user.Id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (repo *userRepository) Delete(id int64) error {
+	_, err := repo.db.Exec("delete from users where id=$1", id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
