34a2716bdc5605ee5e2c9a845d7212ad743a15ef
Aibek <aibek.abdykasymov@gmail.com>
2020-04-19 21:35:17 +0600

    Added tests for reset token

--- e2e/spec/auth_test.go
+++ e2e/spec/auth_test.go
@@ -203,5 +203,92 @@ var _ = Describe("Auth", func() {
 		})
 
 	})
+	Context("User reset password", func(){
+		BeforeEach(func(){
+			url = fmt.Sprintf("%s/auth/reset-password", Server.URL)
+		})
+		It("User reset password successfully", func(){
+			emailPassword := map[string]string{
+				"email": "first_user@test.test",
+				"password": "first_user_password",
+				"token": "fc5c078c673b9799e9a9b6a95531d77c",
+			}
+			data, err := json.Marshal(emailPassword)
+			if err != nil {
+				log.Printf("json marshal error: %s", err)
+			}
+			response, err := Client.Post(url, contentType, bytes.NewBuffer(data))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+			expectedBody := map[string]string{
+				"status": "success",
+				"message": "Your password successfully changed.",
+			}
+			Expect(test_utils.GetResponseBodyJson(response)).To(Equal(expectedBody))
+		})
+		It("User reset password failed with json decode error", func(){
+			response, err := Client.Post(url, contentType, bytes.NewBuffer([]byte("")))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusUnprocessableEntity))
+			expectedBody := map[string]string{
+				"error": "EOF",
+			}
+			Expect(test_utils.GetResponseBodyJson(response)).To(Equal(expectedBody))
+		})
+		It("User reset failed with json validate error", func() {
+			emailPassword := map[string]string{
+				"email": "first_user.test.test",
+				"password": "",
+				"token": "",
+			}
+			data, err := json.Marshal(emailPassword)
+			if err != nil {
+				log.Printf("json marshal error: %s", err)
+			}
+			response, err := Client.Post(url, contentType, bytes.NewBuffer(data))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusUnprocessableEntity))
+			expectedBody := map[string]string{
+				"error": "Key: 'EmailPasswordToken.Password' Error:Field validation for 'Password' failed on the 'required' tag\nKey: 'EmailPasswordToken.Token' Error:Field validation for 'Token' failed on the 'required' tag",
+			}
+			Expect(test_utils.GetResponseBodyJson(response)).To(Equal(expectedBody))
+		})
+		It("User reset failed with email not found error", func() {
+			emailPassword := map[string]string{
+				"email": "wrong@test.test",
+				"password": "first_user_password",
+				"token": "fc5c078c673b9799e9a9b6a95531d77c",
+			}
+			data, err := json.Marshal(emailPassword)
+			if err != nil {
+				log.Printf("json marshal error: %s", err)
+			}
+			response, err := Client.Post(url, contentType, bytes.NewBuffer(data))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusUnprocessableEntity))
+			expectedBody := map[string]string{
+				"error": "sql: no rows in result set",
+			}
+			Expect(test_utils.GetResponseBodyJson(response)).To(Equal(expectedBody))
+		})
+		It("User reset failed with token validate error", func() {
+			emailPassword := map[string]string{
+				"email": "first_user@test.test",
+				"password": "first_user_password",
+				"token": "wrong_token",
+			}
+			data, err := json.Marshal(emailPassword)
+			if err != nil {
+				log.Printf("json marshal error: %s", err)
+			}
+			response, err := Client.Post(url, contentType, bytes.NewBuffer(data))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusUnprocessableEntity))
+			expectedBody := map[string]string{
+				"error": "The token is wrong.",
+			}
+			Expect(test_utils.GetResponseBodyJson(response)).To(Equal(expectedBody))
+		})
+	})
 
 })
